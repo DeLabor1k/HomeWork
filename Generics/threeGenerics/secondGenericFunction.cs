976ca116da9a8b9f630fb26ac665ebfc088b56ef
DeLabor <cotrellyt@gmail.com>
2026-05-16 14:32:39 +0700

    Update secondGenericFunction.cs

--- Generics/threeGenerics/secondGenericFunction.cs
+++ Generics/threeGenerics/secondGenericFunction.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
 
-public static void FillWithCats(List<Animal> dst)
+public static void FillWithCats<T>(List<T> dst) where T : Animal
 {
     dst.Clear();
-    dst.Add(new Cat("Cat 1"));
-    dst.Add(new Cat("Cat 2"));
-    dst.Add(new Cat("Cat 3"));
+    dst.Add((T)(object)new Cat("Cat 1"));
+    dst.Add((T)(object)new Cat("Cat 2"));
+    dst.Add((T)(object)new Cat("Cat 3"));
 }
